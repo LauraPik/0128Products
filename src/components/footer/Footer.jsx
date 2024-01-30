7021285610388d0173b0beaa102d210115375de0
Laura <laura.pikele@stud.kitm.lt>
2024-01-30 22:23:05 +0200

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,16 +4,10 @@ const Footer = () =>{
 
     return(
         <>
-        <footer >
+        <footer  className="py-11 bg-black text-white text-center">
            
-           <div className="container-fluid footer-container-fluid">
-               <div className="container footer-container">
-                   <p href="#" className="copyright">React projektinis darbas</p>
-                   
-                   
-                   </div>
-                   
-           </div>
+           React projektinis darbas
+        
        </footer>
 
         </>
